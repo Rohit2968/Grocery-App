06b852a628d71facbdc26b669d9d40262dd4b05e
Rohit2968 <rohit2968raj@gmail.com>
2025-07-09 12:31:26 +0530

    Implemented Navbar component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import ProductDetails from "./pages/ProductDetails";
+import Cart from "./pages/Cart";
+import Navbar from "./components/Navbar";
+import { AppContext } from "./context/AppContext";
 
 const App = () => {
+  const { isSeller } = useContext(AppContext);
+  // "/seller" route -> No Navbar 
+  const isSellerPath = useLocation().pathname.includes("seller");
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      {isSellerPath ? null : <Navbar />}
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/product/:id" element={<ProductDetails />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </div>
+    </div>
+  );
+};
 
 export default App;
