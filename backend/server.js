5124c5343a7dfac2b92673c5a12a3924bb64cea6
Rohit2968 <rohit2968raj@gmail.com>
2025-08-03 16:47:14 +0530

    backend setup

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+// backend/server.js
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+// const connectDB = require("./config/db");
+
+// dotenv.config();
+// connectDB();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Routes
+// app.use("/api/seller", require("./routes/sellerRoutes"));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
